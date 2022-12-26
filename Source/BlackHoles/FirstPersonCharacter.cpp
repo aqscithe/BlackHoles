3bf4663e91d2f95138926492597cf12d2e92cffd
Leon Thompson <ltyronethompson09@gmail.com>
2022-12-26 11:19:18 -0600

    basic fp char movement

--- Source/BlackHoles/FirstPersonCharacter.cpp
+++ Source/BlackHoles/FirstPersonCharacter.cpp
@@ -4,6 +4,9 @@
 #include "FirstPersonCharacter.h"
 
 #include "Camera/CameraComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputMappingContext.h"
 
 // Sets default values
 AFirstPersonCharacter::AFirstPersonCharacter()
@@ -13,6 +16,8 @@ AFirstPersonCharacter::AFirstPersonCharacter()
 
 	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("FP Camera"));
 	Camera->SetupAttachment(RootComponent);
+	Camera->bUsePawnControlRotation = true;
+
 
 	ProjectileSpawnPoint = CreateDefaultSubobject<USceneComponent>(TEXT("Projectile Spawn"));
 	ProjectileSpawnPoint->SetupAttachment(RootComponent);
@@ -24,6 +29,19 @@ AFirstPersonCharacter::AFirstPersonCharacter()
 void AFirstPersonCharacter::BeginPlay()
 {
 	Super::BeginPlay();
+
+	if (APlayerController* PlayerController = Cast<APlayerController>(GetController()))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* InputSystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			if (!InputMapping.IsNull())
+			{
+				InputSystem->AddMappingContext(InputMapping.LoadSynchronous(), InputMappingPriority);
+			}
+		}
+	}
+
+	
 	
 }
 
@@ -39,5 +57,39 @@ void AFirstPersonCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInp
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	if (UEnhancedInputComponent* Input = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		Input->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AFirstPersonCharacter::Move);
+		Input->BindAction(LookAction, ETriggerEvent::Triggered, this, &AFirstPersonCharacter::Look);
+	}
 }
 
+// Remember FInputActionInstance has access to more data
+// Use FInputActionValue if that is all you need
+void AFirstPersonCharacter::Move(const FInputActionValue& Value)
+{
+	const FVector2D MovementVector = Value.Get<FVector2D>();
+
+	const FRotator Rotation = GetController()->GetControlRotation();
+	const FRotator YawRotation(0.f, Rotation.Yaw, 0.f);
+
+
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	AddMovementInput(ForwardDirection, MovementVector.Y );
+
+	const FVector SideDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+	AddMovementInput(SideDirection, MovementVector.X);
+
+
+	UE_LOG(LogTemp, Display, TEXT("Movement Vector: (%f, %f)"), MovementVector.X, MovementVector.Y);
+}
+
+void AFirstPersonCharacter::Look(const FInputActionValue& Value)
+{
+	const FVector2D LookAxisVector = Value.Get<FVector2D>();
+
+	AddControllerPitchInput(LookAxisVector.Y);
+	AddControllerYawInput(LookAxisVector.X);
+
+	UE_LOG(LogTemp, Display, TEXT("Look Axis Vector: (%f, %f)"), LookAxisVector.X, LookAxisVector.Y);
+}
--- Source/BlackHoles/FirstPersonCharacter.h
+++ Source/BlackHoles/FirstPersonCharacter.h
@@ -6,6 +6,9 @@
 #include "GameFramework/Character.h"
 #include "FirstPersonCharacter.generated.h"
 
+
+class UInputAction;
+
 UCLASS()
 class BLACKHOLES_API AFirstPersonCharacter : public ACharacter
 {
@@ -33,5 +36,38 @@ public:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+
+	UFUNCTION()
+	void Move(const FInputActionValue& Value);
+
+	UFUNCTION()
+	void Look(const FInputActionValue& Value);
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TSoftObjectPtr<class UInputMappingContext> InputMapping;
+
+	int32 InputMappingPriority = 0;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	const UInputAction* MoveAction;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	const UInputAction* LookAction;
+
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float ForwardBackSpeed = 200.f;
+
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float SideSpeed = 200.f;
+	
+
+	//void MoveRight(float AxisValue);
+	//void LookRight(float AxisValue);
+	//void LookUp(float AxisValue);
+	//
+	//void LookRightRate(float AxisValue);
+	//void LookUpRate(float AxisValue);
 
 };

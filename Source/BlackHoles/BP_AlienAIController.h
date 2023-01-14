f89dbef658d97092d941c98394dd6eadec96d0c9
Leon Thompson <ltyronethompson09@gmail.com>
2023-01-14 06:28:11 -0600

    prepping manager class for objects pulled in by black holes

---
+++ Source/BlackHoles/BP_AlienAIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "BP_AlienAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLACKHOLES_API ABP_AlienAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
